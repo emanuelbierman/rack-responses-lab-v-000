dd4bb1f9e677fc639435f3ff552fa8ef7187555a
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-08-09 14:07:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ class Application
     resp = Rack::Response.new
 
     time = Time.now
-    
-    if
+
+    if time > 12
       resp.write "Good Morning"
     else
       resp.write "Good Afternoon"
